507fb6400c087f884925288487d9f35e69ba721d
Michał <michalgadzinski@gmail.com>
2025-03-28 19:26:16 +0100

    only BMI - safe with zero

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,12 @@ use uom::si::f64::{Length, Mass};
 use uom::si::length::meter;
 use uom::si::mass::kilogram;
 
-pub fn bmi(weight: Mass, height: Length) -> f64 {
+pub fn bmi(weight: Mass, height: Length) -> Result<f64, &'static str> {
     let height = height.get::<meter>();
-    weight.get::<kilogram>() / (height * height)
+    if height <= 0.0  {
+        return Err("Height must be greater than zero.");
+    }
+    Ok(weight.get::<kilogram>() / (height * height))
 }
 
 #[cfg(test)]
@@ -14,6 +17,12 @@ mod tests {
     #[test]
     fn shall_calculate_bmi_when_called_bmi() {
         let result = bmi(Mass::new::<kilogram>(60.0), Length::new::<meter>(2.00));
-        assert_eq!(result, 15.0);
+        assert_eq!(result, Ok(15.0));
+    }
+    #[test]
+    fn shall_no_tcalculate_bmi_when_height_zero() {
+        let result = bmi(Mass::new::<kilogram>(60.0), Length::new::<meter>(0.00));
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "Height must be greater than zero.");
     }
 }
